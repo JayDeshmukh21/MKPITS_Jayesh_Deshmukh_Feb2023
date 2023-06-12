b5727cc0f6eddcb10e711bf92251ccb0f3f0ace9
JayDeshmukh21 <jaydeshmukh1021@gmail.com>
2023-06-12 18:35:57 +0530

    updated

---
+++ ASP.NET Program/ADO1/ADO1/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="ADO1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            margin-left: 80px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="auto-style1">
+            <asp:GridView ID="GridView3" runat="server" Height="128px" Width="344px">
+            </asp:GridView>
+            <br />
+            Total Amount : 
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />Amount Paid :
+            <asp:TextBox ID="TextBox2" runat="server" Width="224px"></asp:TextBox>
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Load" />
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="RadioButton1" runat="server" Text="CASH" />
+&nbsp;&nbsp;&nbsp;
+            <asp:RadioButton ID="RadioButton2" runat="server" Text="EMI" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <br />
+            <br />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Show Emi" />
+            <br />
+            <asp:GridView ID="GridView2" runat="server" Width="352px">
+            </asp:GridView>
+            <br />
+            <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ ASP.NET Program/ADO1/ADO1/WebForm1.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ADO1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable("dtable");
+            DataRow dr;
+            dt.Columns.Add(new DataColumn("Srno",typeof(int)));
+            dt.Columns.Add(new DataColumn("name",typeof(string)));
+            dt.Columns.Add(new DataColumn("doa", typeof(DateTime)));
+            dr = dt.NewRow();
+            dr[0] = 1;
+            dr[1] = "Shiva";
+            dr[2] = DateTime.Now;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = 2;
+            dr[1] = "Jayesh";
+            dr[2]= DateTime.Now;
+            dt.Rows.Add(dr);
+
+            dr = dt.NewRow();
+            dr[0] = 3;
+            dr[1] = "Raghav";
+            dr[2] = DateTime.Now;
+            dt.Rows.Add(dr);
+
+            ds.Tables.Add(dt);
+            GridView3.DataSource = ds.Tables["dtable"].DefaultView;
+            GridView3.DataBind();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            int tamount = Convert.ToInt32(TextBox1.Text);
+            int pamount = Convert.ToInt32(TextBox2.Text);
+            int ramount = tamount - pamount;
+            double eamount = 0;
+
+            if(RadioButton1.Checked)
+            {
+                if(ramount >0)
+                {
+                    Label1.Text = "Please pay full amount in cash";
+                }
+                else if(RadioButton2.Checked)
+                {
+                    if(ramount > 0)
+                    {
+                        eamount = ramount / 3.0 ;
+
+                    }
+                    string pname = "Monster";
+                    DataSet ds = new DataSet();
+                    DataTable dt = new DataTable("Emitable");
+                    DataRow dr;
+                    dt.Columns.Add(new DataColumn("srno", typeof(int)));
+                    dt.Columns.Add(new DataColumn("Productname", typeof(string)));
+                    dt.Columns.Add(new DataColumn("Emi Amount", typeof(decimal)));
+
+                    for (int i = 1; i<= 3; i++)
+                    {
+                        dr = dt.NewRow();
+                        dr[0] = i;
+                        dr[1] = pname;
+                        dr[2] = eamount;
+                        dt.Rows.Add(dr);
+                    }
+                    ds.Tables.Add(dt);
+                    GridView2.DataSource = ds.Tables["emitable"].DefaultView;
+                    GridView2.DataBind();
+                }
+            }
+        }
+    }
+}
