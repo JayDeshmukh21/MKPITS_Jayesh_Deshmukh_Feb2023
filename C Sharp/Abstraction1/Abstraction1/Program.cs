9b74f08c6b4c0cbad2938473c79650f1cfab987d
JayDeshmukh21 <jaydeshmukh1021@gmail.com>
2023-06-28 23:12:09 +0530

    uploaded

---
+++ C Sharp/Abstraction1/Abstraction1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+abstract class Shape
+{
+    public abstract void Draw();
+}
+
+class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle.");
+    }
+}
+
+class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape[] shapes = new Shape[2];
+        shapes[0] = new Circle();
+        shapes[1] = new Rectangle();
+
+        foreach (Shape shape in shapes)
+        {
+            shape.Draw();
+        }
+        Console.ReadLine();
+    }
+}
---
+++ C Sharp/Inheritance1/Inheritance1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Vehicle
+{
+    public string Brand { get; set; }
+    public string Model { get; set; }
+
+    public void StartEngine()
+    {
+        Console.WriteLine("Engine started.");
+    }
+
+    public void StopEngine()
+    {
+        Console.WriteLine("Engine stopped.");
+    }
+}
+
+class Car : Vehicle
+{
+    public void Accelerate()
+    {
+        Console.WriteLine("Car accelerated.");
+    }
+
+    public void Brake()
+    {
+        Console.WriteLine("Car braked.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Car myCar = new Car();
+        myCar.Brand = "Toyota";
+        myCar.Model = "Camry";
+        myCar.StartEngine();
+        myCar.Accelerate();
+        myCar.Brake();
+        myCar.StopEngine();
+        Console.ReadLine();
+    }
+}
