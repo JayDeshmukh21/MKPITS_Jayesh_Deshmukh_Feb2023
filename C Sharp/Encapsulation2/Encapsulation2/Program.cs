ab97cee2691b7d9e8acbf6b681c1359624dcfac7
JayDeshmukh21 <jaydeshmukh1021@gmail.com>
2023-06-30 21:57:54 +0530

    updl

---
+++ C Sharp/Encapsulation2/Encapsulation2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class BankAccount
+{
+    private double balance;
+
+    public void Deposit(double amount)
+    {
+        balance += amount;
+        Console.WriteLine($"Deposited {amount}. New balance: {balance}");
+    }
+
+    public void Withdraw(double amount)
+    {
+        if (amount <= balance)
+        {
+            balance -= amount;
+            Console.WriteLine($"Withdrawn {amount}. New balance: {balance}");
+        }
+        else
+        {
+            Console.WriteLine("Insufficient funds!");
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        BankAccount account = new BankAccount();
+        account.Deposit(1000);
+        account.Withdraw(500);
+        account.Withdraw(800);
+        Console.ReadLine();
+    }
+}
