7e9737fe8da316df7e0a9220dbe96925912c38a4
JayDeshmukh21 <jaydeshmukh1021@gmail.com>
2023-07-01 22:51:57 +0530

    uploaded

---
+++ C Sharp/polymorphism5/polymorphism5/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class Shape
+{
+    public virtual void Draw()
+    {
+        Console.WriteLine("Drawing a shape.");
+    }
+}
+
+class Circle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a circle.");
+    }
+}
+
+class Rectangle : Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Shape();
+        shapes[1] = new Circle();
+        shapes[2] = new Rectangle();
+
+        foreach (Shape shape in shapes)
+        {
+            shape.Draw();
+        }
+        Console.ReadLine();
+
+    }
+}
