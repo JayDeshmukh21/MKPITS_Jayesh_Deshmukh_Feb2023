b970ef860ea0a0abfc8db8110298a22b7dbbed86
JayDeshmukh21 <jaydeshmukh1021@gmail.com>
2023-07-10 23:13:14 +0530

    uploaded

---
+++ MVC Proj/formcollection2/formcollection2/Models/StudentModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace formcollection2.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool Addon { get; set; }
+
+    }
+}
